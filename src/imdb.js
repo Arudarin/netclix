a70cab7e7aa8cbbd30b9171e0af7efbaf6e75727
Arudarin <koolboync@gmail.com>
2018-12-25 11:36:27 -0500

    Convert 'movie' to 'title'
    Makes more sense if both movies and tv is eventually implemented

--- src/imdb.js
+++ src/imdb.js
@@ -3,10 +3,10 @@ import Horseman from 'node-horseman';
 
 export default async search => new Horseman(phantomjs).open(`http://www.imdb.com/find?q=${search}&s=tt&ttype=ft`)
     .evaluate(() => {
-        const movies = [].slice.call(document.querySelectorAll('.result_text a')).slice(0, 10);
-        return movies.map(movie => ({
-            name: movie.text,
-            value: movie.text,
+        const titles = [].slice.call(document.querySelectorAll('.result_text a')).slice(0, 10);
+        return titles.map(title => ({
+            name: title.text,
+            value: title.text,
             // value: movie.href.match(/tt[0-9]+/)[0],
         }));
     })
